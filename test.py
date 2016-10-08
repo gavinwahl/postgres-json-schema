dd8b9828ba1500352b917b370f51e6797e149bfd
Gavin Wahl <gavinwahl@gmail.com>
2016-10-07 22:22:36 -0600

    JSON Schema validation in postgres

---
+++ test.py
@@ -0,0 +1,47 @@
+import os
+import json
+import sys
+
+import psycopg2
+
+
+conn = psycopg2.connect(os.environ['DATABASE_URL'])
+conn.set_session(autocommit=True)
+
+cur = conn.cursor()
+
+with open('jsonschema.sql') as f:
+    cur.execute(f.read())
+
+with open('tests.sql') as f:
+    cur.execute(f.read())
+
+EXCLUDE = {'optional', 'refRemote.json', 'definitions.json'}
+
+os.chdir('JSON-Schema-Test-Suite/tests/draft4')
+failures = 0
+
+test_files = sys.argv[1:]
+if not test_files:
+    test_files = [test_file for test_file in os.listdir('.') if test_file not in EXCLUDE]
+
+for test_file in test_files:
+    with open(test_file) as f:
+        suites = json.load(f)
+        for suite in suites:
+            for test in suite['tests']:
+                def fail(e):
+                    print("%s: validate_json_schema('%s', '%s')" % (test_file, json.dumps(suite['schema']), json.dumps(test['data'])))
+                    print('Failed: %s: %s. %s' % (suite['description'], test['description'], e))
+                try:
+                    cur.execute('SELECT validate_json_schema(%s, %s)', (json.dumps(suite['schema']), json.dumps(test['data'])))
+                except psycopg2.DataError as e:
+                    fail(e)
+                    failures += 1
+                else:
+                    valid, = cur.fetchone()
+                    if valid != test['valid']:
+                        fail(valid)
+                        failures += 1
+
+sys.exit(failures)
---
+++ tests.sql
@@ -0,0 +1,62 @@
+CREATE OR REPLACE FUNCTION run_tests() RETURNS boolean AS $f$
+BEGIN
+  ASSERT validate_json_schema('{}', '{}');
+  ASSERT NOT validate_json_schema('{"type": "object"}', '1');
+  ASSERT validate_json_schema('{"type": "object"}', '{}');
+  ASSERT validate_json_schema('{"type": "number"}', '123.1');
+  ASSERT validate_json_schema('{"type": "number"}', '123');
+  ASSERT validate_json_schema('{"type": "integer"}', '123');
+  ASSERT NOT validate_json_schema('{"type": "integer"}', '123.1');
+  ASSERT NOT validate_json_schema('{"type": "number"}', '"a"');
+  ASSERT validate_json_schema('{"type": "string"}', '"a"');
+  ASSERT NOT validate_json_schema('{"type": "string"}', '{}');
+  ASSERT validate_json_schema('{"type": "boolean"}', 'true');
+  ASSERT NOT validate_json_schema('{"type": "boolean"}', 'null');
+  ASSERT validate_json_schema('{"type": "null"}', 'null');
+  ASSERT NOT validate_json_schema('{"type": "null"}', 'true');
+  ASSERT validate_json_schema('{"type": "boolean"}', 'false');
+  ASSERT NOT validate_json_schema('{"type": "boolean"}', '[]');
+  ASSERT validate_json_schema('{"type": "array"}', '[]');
+  ASSERT NOT validate_json_schema('{"type": "array"}', '1');
+
+  ASSERT validate_json_schema('{"properties": {"foo": {"type": "string"}}}', '{"foo": "bar"}');
+  ASSERT NOT validate_json_schema('{"properties": {"foo": {"type": "string"}}}', '{"foo": 1}');
+  ASSERT validate_json_schema('{"properties": {"foo": {"type": "string"}}}', '{}');
+
+  ASSERT validate_json_schema('{"required": ["foo"]}', '{"foo": 1}');
+  ASSERT NOT validate_json_schema('{"required": ["foo"]}', '{"bar": 1}');
+
+  ASSERT validate_json_schema('{"items": {"type": "integer"}}', '[1, 2, 3]');
+  ASSERT NOT validate_json_schema('{"items": {"type": "integer"}}', '[1, 2, 3, "x"]');
+
+  ASSERT validate_json_schema('{"minimum": 1.1}', '2.6');
+  ASSERT NOT validate_json_schema('{"minimum": 1.1}', '0.6');
+  ASSERT validate_json_schema('{"minimum": 12}', '12');
+
+  ASSERT validate_json_schema('{"anyOf": [{"type": "integer"}, {"minimum": 2}]}', '1');
+  ASSERT validate_json_schema('{"anyOf": [{"type": "integer"}, {"minimum": 2}]}', '2.5');
+  ASSERT validate_json_schema('{"anyOf": [{"type": "integer"}, {"minimum": 2}]}', '3');
+  ASSERT NOT validate_json_schema('{"anyOf": [{"type": "integer"}, {"minimum": 2}]}', '1.5');
+
+  ASSERT validate_json_schema('{"uniqueItems": true}', '[1, 2]');
+  ASSERT NOT validate_json_schema('{"uniqueItems": true}', '[1, 1]');
+
+  ASSERT validate_json_schema('{"properties": {"foo": {}, "bar": {}}, "additionalProperties": false}', '{"foo": 1}');
+  ASSERT NOT validate_json_schema('{"properties": {"foo": {}, "bar": {}}, "additionalProperties": false}', '{"foo" : 1, "bar" : 2, "quux" : "boom"}');
+
+  ASSERT validate_json_schema('{"properties": {"foo": {"$ref": "#"}}, "additionalProperties": false}', '{"foo": false}');
+  ASSERT validate_json_schema('{"properties": {"foo": {"$ref": "#"}}, "additionalProperties": false}', '{"foo": {"foo": false}}');
+  ASSERT NOT validate_json_schema('{"properties": {"foo": {"$ref": "#"}}, "additionalProperties": false}', '{"bar": false}');
+  ASSERT NOT validate_json_schema('{"properties": {"foo": {"$ref": "#"}}, "additionalProperties": false}', '{"foo": {"bar": false}}');
+
+  ASSERT validate_json_schema('{"properties": {"foo": {"type": "integer"}, "bar": {"$ref": "#/properties/foo"}}}', '{"bar": 3}');
+  ASSERT NOT validate_json_schema('{"properties": {"foo": {"type": "integer"}, "bar": {"$ref": "#/properties/foo"}}}', '{"bar": true}');
+
+  ASSERT validate_json_schema('{"enum": [1,2,3]}', '1');
+  ASSERT NOT validate_json_schema('{"enum": [1,2,3]}', '4');
+
+  RETURN true;
+END;
+$f$ LANGUAGE 'plpgsql';
+
+SELECT run_tests();
